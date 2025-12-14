577bbcc60ae231f6152a7fa48dd513551959ab03
Ankur <ankurpathak795@gmail.com>
2025-12-14 20:57:40 +0530

    general syntax errors

--- src/features/auth/components/auth-layout.tsx
+++ src/features/auth/components/auth-layout.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React,  from 'react'
+import React  from 'react'
 
 const AuthLayout = ({children} :{children : React.ReactNode; }) => {
   return (
