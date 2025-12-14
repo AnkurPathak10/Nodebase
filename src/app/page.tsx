deade6a6e06807804fccc4232c191e70e7554864
Ankur <ankurpathak795@gmail.com>
2025-12-14 13:19:12 +0530

    added auth procedures (tRPC)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 
 import { requireAuth } from "@/lib/auth-utils"
+import { caller } from "@/trpc/server";
 
 
 
 const Page = async() => {
   await requireAuth();
 
+  const data = await caller.getUsers();
+
   return (
     <div className='min-h-screen min-w-screen flex items-center justify-center'>
       Protected server component
+      {JSON.stringify(data)}
     </div>
   )
 }
