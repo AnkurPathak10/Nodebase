132591021a48f970c41a49cdc6412f4d272146ac
Ankur <ankurpathak795@gmail.com>
2025-12-16 16:37:31 +0530

    Error tracking with senetry

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,5 +1,6 @@
 
 import { inngest } from "./client";
+import * as Sentry from "@sentry/nextjs";
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
 import { generateText } from 'ai';
 import { createOpenAI } from '@ai-sdk/openai';
@@ -15,26 +16,45 @@ export const execute = inngest.createFunction(
 
   async ({ event, step }) => {
     await step.sleep("pretend", "5s");
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' })
 
+    console.warn("Something is missing");
+    console.error("This is an error I wanted to track");
+    
     const {steps: geminiSteps} = await step.ai.wrap("gemini-generate-text" ,
       generateText, {
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant.",
-        prompt: "What is 2+2"
+        prompt: "What is 2+2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
     })
 
     const {steps: openaiSteps} = await step.ai.wrap("openai-generate-text" ,
       generateText, {
         model: openai("gpt-5.2"),
         system: "You are a helpful assistant.",
-        prompt: "What is 2+2"
+        prompt: "What is 2+2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
     })
 
     const {steps: anthropicSteps} = await step.ai.wrap("anthropic-generate-text" ,
       generateText, {
         model: anthropic("claude-sonnet-4-5"),
         system: "You are a helpful assistant.",
-        prompt: "What is 2+2"
+        prompt: "What is 2+2",
+        experimental_telemetry: {
+          isEnabled: true,
+          recordInputs: true,
+          recordOutputs: true,
+        },
     })
     return {
       geminiSteps,
