c67e292f45b39e507ca9ded904ac0ff25f82788f
Ankur <ankurpathak795@gmail.com>
2025-12-18 11:07:47 +0530

    set up payments using polar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -12,6 +12,7 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import {Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem} from "@/components/ui/sidebar";
 import { authClient } from "@/lib/auth-client";
+import { useHasActiveSubscription } from "@/features/auth/components/subscription/hooks/use-subscription";
 
 const menuItems = [
    {
@@ -39,6 +40,8 @@ const menuItems = [
 export const AppSidebar = () => {
     const router = useRouter();
     const pathname = usePathname();
+    const {hasActiveSubscription, isLoading} = useHasActiveSubscription();
+
     return (
         <Sidebar collapsible="icon">
             <SidebarHeader>
@@ -83,23 +86,24 @@ export const AppSidebar = () => {
             
             <SidebarFooter>
                 <SidebarMenu>
-                    <SidebarMenuItem>
-                        <SidebarMenuButton 
-                            tooltip="Upgrade to Pro" 
-                            className="gap-x-4 h-10 px-4"
-                            onClick={() => {}}>
-                                <StarIcon className="h-4 w-4"/>
-                                <span>
-                                    Upgrade to Pro
-                                </span>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-
+                    {!hasActiveSubscription && !isLoading && (
+                        <SidebarMenuItem>
+                            <SidebarMenuButton 
+                                tooltip="Upgrade to Pro" 
+                                className="gap-x-4 h-10 px-4"
+                                onClick={() => authClient.checkout({ slug : "pro"})}>
+                                    <StarIcon className="h-4 w-4"/>
+                                    <span>
+                                        Upgrade to Pro
+                                    </span>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    )}
                     <SidebarMenuItem>
                         <SidebarMenuButton 
                             tooltip="Billing Portal" 
                             className="gap-x-4 h-10 px-4"
-                            onClick={() => {}}>
+                            onClick={() => authClient.customer.portal()}>
                                 <CreditCardIcon className="h-4 w-4"/>
                                 <span>
                                     Billing Portal
