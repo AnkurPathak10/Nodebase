8d36f2cff1a6b6c2ea769759721a6df0cb606e6b
Ankur <ankurpathak795@gmail.com>
2025-12-25 13:08:46 +0530

    created full editor of nodes using react flow

---
+++ src/components/initial-node.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import type { NodeProps } from "@xyflow/react";
+import { memo } from "react";
+import { PlaceholderNode } from "./react-flow/placeholder-node";
+import { PlusIcon } from "lucide-react";
+import { WorkflowNode } from "./workflow-node";
+
+export const InitialNode = memo((props: NodeProps) => {
+    return (
+        <WorkflowNode showToolBar={false}>
+            <PlaceholderNode
+                {...props}
+                onClick={() => {}}
+            >
+                <div className="cursor-pointer flex items-center justify-center">
+                    <PlusIcon/>
+                </div>
+            </PlaceholderNode>
+        </WorkflowNode>
+    )
+})
