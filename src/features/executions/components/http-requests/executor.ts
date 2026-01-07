f2a22a95d0744d05f0140b28d59ce1b5b35eb065
Ankur <ankurpathak795@gmail.com>
2026-01-07 21:32:23 +0530

    created realtime (loacing , success, fail) functionalities for nodes

--- src/features/executions/components/http-requests/executor.ts
+++ src/features/executions/components/http-requests/executor.ts
@@ -2,6 +2,7 @@ import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, {type Options as KyOptions} from "ky";
 import Handlebars from "handlebars";
+import { httpRequestChannel } from "@/inngest/channels/http-request";
 
 Handlebars.registerHelper("json", (context) => {
     const jsonString = JSON.stringify(context, null, 2);
@@ -21,61 +22,110 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
     data,
     nodeId,
     context,
-    step
+    step,
+    publish,
 }) => {
     //Publish loading state for HttpRequest
+    await publish(
+        httpRequestChannel().status({
+            nodeId,
+            status: "loading",
+        })
+    )
+
     if(!data.endpoint){
         //publish error state for http request
-
+        await publish(
+            httpRequestChannel().status({
+                nodeId,
+                status: "error",
+            })
+        )
         throw new NonRetriableError("HTTP Request node: No endpoint configured");
     }
 
     if(!data.variableName){
-        
+        await publish(
+            httpRequestChannel().status({
+                nodeId,
+                status: "error",
+            })
+        )
         throw new NonRetriableError("Variable name not configured");
     }
 
     if(!data.method){
-        
+        await publish(
+            httpRequestChannel().status({
+                nodeId,
+                status: "error",
+            })
+        )
         throw new NonRetriableError("Method not configured");
     }
 
-    const result = await step.run("http-request", async () => {
+    try{
+        const result = await step.run("http-request", async () => {
 
-        const endpoint = Handlebars.compile(data.endpoint)(context);
-        const method = data.method;
+            const endpoint = Handlebars.compile(data.endpoint)(context);
+            const method = data.method;
 
-        const options: KyOptions = {method};
+            const options: KyOptions = {method};
 
-        if(["POST","PUT","PATCH"].includes(method)){
-            const resolved = Handlebars.compile(data.body || "{}")(context);
-            JSON.parse(resolved);
-            options.body = resolved;
-            options.headers = {
-                "Content-Type": "application/json",
-            };
-        }
+            if (["POST", "PUT", "PATCH"].includes(method)) {
+                const resolved = Handlebars.compile(data.body || "{}")(context);
+              
+                try {
+                  JSON.parse(resolved);
+                } catch (err) {
+                  throw new NonRetriableError(
+                    `Invalid JSON body: ${(err as Error).message}`
+                  );
+                }
+              
+                options.body = resolved;
+                options.headers = {
+                  "Content-Type": "application/json",
+                };
+              }
+              
 
-        const response = await ky(endpoint, options);
-        const contentType = response.headers.get("content-type");
-        const responseData = contentType?.includes("application/json") ? await response.json() : await response.text();
-        const responsePayload = {
-            httpResponse: {
-                status: response.status,
-                statusText: response.statusText,
-                data: responseData,
-            },
-        }
-        
-        return {
-            ...context,
-            [data.variableName]: responsePayload,
-        }
-    });
+            const response = await ky(endpoint, options);
+            const contentType = response.headers.get("content-type");
+            const responseData = contentType?.includes("application/json") ? await response.json() : await response.text();
+            const responsePayload = {
+                httpResponse: {
+                    status: response.status,
+                    statusText: response.statusText,
+                    data: responseData,
+                },
+            }
+            
+            return {
+                ...context,
+                [data.variableName]: responsePayload,
+            }
+        });
 
 
 
-    //Publish success state for HttpRequest
+        //Publish success state for HttpRequest
+        await publish(
+            httpRequestChannel().status({
+                nodeId,
+                status: "success",
+            })
+        )
 
-    return result;
+
+        return result;
+    } catch(error){  
+        await publish(
+            httpRequestChannel().status({
+                nodeId,
+                status: "error",
+            })
+        )
+        throw error;
+    }
 };
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,12 +4,23 @@ import prisma from "@/lib/db";
 import { topologicalSort } from "./utils";
 import { NodeType } from "@/generated/prisma/enums";
 import { getExecutor } from "@/features/executions/lib/executor-registry";
+import { httpRequestChannel } from "./channels/http-request";
+import { manualTriggerChannel } from "./channels/manual-trigger";
 
 export const executeWorkflow = inngest.createFunction(
-  { id: "execute-workflow" },
-  { event: "workflows/execute.workflow" },
+  { 
+    id: "execute-workflow",
+    retries: 0,
+  },
+  { 
+    event: "workflows/execute.workflow",
+    channels: [
+      httpRequestChannel(),
+      manualTriggerChannel(),
+    ],
+  },
 
-  async ({ event, step }) => {
+  async ({ event, step, publish }) => {
     const workflowId = event.data.workflowId;
 
     if(!workflowId){
@@ -38,6 +49,7 @@ export const executeWorkflow = inngest.createFunction(
         nodeId: node.id,
         context,
         step,
+        publish,
       })
     }
 
